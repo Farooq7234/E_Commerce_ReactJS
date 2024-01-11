740a80fc51ed4fb42edd3901f585f4a0307b8e50
Umar farooq <ufservices465@gmail.com>
2024-01-11 20:34:41 +0530

    code optimized

--- src/App.jsx
+++ src/App.jsx
@@ -1,104 +1,89 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './component/Header'
+import Header from './component/Header';
 import HeroSection from './component/HeroSection';
 import Service from './component/Service';
-import { FaTruck } from "react-icons/fa6";
-import { PiCertificateFill } from "react-icons/pi";
-import { FaMoneyBillAlt } from "react-icons/fa";
-import { RiRecycleFill } from "react-icons/ri";
+import { FaTruck, FaMoneyBillAlt } from 'react-icons/fa';
+import { PiCertificateFill } from 'react-icons/pi';
+import { RiRecycleFill } from 'react-icons/ri';
 import TrendProduct from './component/TrendProduct';
-import { FaRegStar } from "react-icons/fa";
 import FeatureSection from './component/FeatureSection';
 import OfferSection from './component/OfferSection';
 import CustomerReview from './component/CustomerReview';
 import LogoContainer from './component/LogoContainer';
 import Footer from './component/Footer';
-import { FaReact } from "react-icons/fa";
 
+const SERVICE_DATA = [
+  { value1: 'Free Shipping', value2: 'Above $5 Only', icon: <FaTruck className='text-3xl text-[#8bc34a]' /> },
+  { value1: 'Certified Organic', value2: '100% Guarantee', icon: <PiCertificateFill className='text-3xl text-[#8bc34a]' /> },
+  { value1: 'Huge Savings', value2: 'At Lowest Price', icon: <FaMoneyBillAlt className='text-3xl text-[#8bc34a]' /> },
+  { value1: 'Easy Return', value2: 'No Question Asked', icon: <RiRecycleFill className='text-3xl text-[#8bc34a]' /> },
+];
 
+const PRODUCT_DATA = {
+  'Assorted Coffee': { category: 'Groceries', img: 'coffee-asorted', price: 35.00 },
+  'Hand Sanitizer': { category: 'Juice', img: 'sanitizer', price: 45.00 },
+  'Handpicked redchillies': { category: 'Groceries', img: 'red-chillies', price: 45.00 },
+  'Natural Extracted Edible Oil': { category: 'Groceries', img: 'edible-oil', price: 95.00 },
+};
 
+const FEATURE_DATA = [
+  { product: 'farm fresh fruits', img: 'th.bing.com/th/id/OIP.D-1bRjZ4nMhhkZZuVkKXbgAAAA?rs=1&pid=ImgDetMain' },
+  { product: 'Fresh Vegetables', img: 'th.bing.com/th/id/OIP.D-1bRjZ4nMhhkZZuVkKXbgAAAA?rs=1&pid=ImgDetMain' },
+  { product: 'Organic Legume', img: 'th.bing.com/th/id/OIP.D-1bRjZ4nMhhkZZuVkKXbgAAAA?rs=1&pid=ImgDetMain' },
+  // Add more feature data as needed
+];
 
+const CUSTOMER_REVIEW_DATA = [
+  { custName: 'Mila Kunis', clients: 'client02' },
+  { custName: 'Loki', clients: 'client01' },
+  { custName: 'Loki', clients: 'client02' },
+  // Add more customer review data as needed
+];
 
 function App() {
-
-  const [price,setPrice]= useState(0.00)
-  const [cartValue,setCartvalue]= useState(0)
-
-
-  // services data
-
-  const delivery = "Free Shipping"
-  const delCon = "Above $5 Only"
-
-  const certificate = "Certified Organic"
-  const guarantee = "100% Guarantee"
-
-  const savings = "Huge Savings"
-  const cost = "At Lowest Price"
-
-  const returns = "Easy Return"
-  const questions = "No Question Asked"
-
-  const category = ['Groceries', 'Juice']
-  const productName = ['Assorted Coffee', 'Hand Sanitizer', 'Handpicked redchillies', 'Extracted Edible Oil']
-  const img = ['coffee-asorted','sanitizer', 'red-chillies', 'edible-oil']
-
-  const productPrice={
-    'Assorted Coffee':35.00,
-    'Hand Sanitizer':45.00,
-    'Handpicked redchillies':45.00,
-    'Natural Extracted Edible Oil':95.00,
-  }
-
-  // featured section data here
-
-  const products = ['farm fresh fruits', 'fresh vegetables', 'fresh legume']
-  const productImg = ['th.bing.com/th/id/OIP.D-1bRjZ4nMhhkZZuVkKXbgAAAA?rs=1&pid=ImgDetMain','www.bing.com/images/blob?bcid=S8Z9vF0LQYkGjCt7lmuJ.7helLDF.....-I', 'www.bing.com/images/blob?bcid=S4oaeoM.A4kGjCt7lmuJ.7helLDF.....y8']
-
-
-  // Customer Review Sections data 
-
-  const custName = ['Mila Kunis','Loki']
-  const clients = ['client01','client02']
-
+  const [price, setPrice] = useState(0.00);
+  const [cartValue, setCartValue] = useState(0);
 
   return (
     <>
-    <Router>
-    <Header price={price} cartvalue = {cartValue} setCartvalue = {setCartvalue} />
-    </Router>    
-    <HeroSection/>
-    <div className='flex flex-row max-sm:flex-col'>
-    <Service  value1={delivery} value2 = {delCon} value3={<FaTruck className='text-3xl text-[#8bc34a]' />}/>
-    <Service value1={certificate}  value2={guarantee}  value3={<PiCertificateFill className='text-3xl text-[#8bc34a]' />}  />
-    <Service value1= {savings} value2={cost } value3={<FaMoneyBillAlt  className='text-3xl text-[#8bc34a]' />} />
-    <Service value1={returns} value2={questions}  value3={<RiRecycleFill className='text-3xl text-[#8bc34a]' />}/>
-    </div>
+      <Router>
+        <Header price={price} cartvalue={cartValue} setCartvalue={setCartValue} />
+      </Router>
+      <HeroSection />
+      <div className='flex flex-row max-sm:flex-col'>
+        {SERVICE_DATA.map(({ value1, value2, icon }, index) => (
+          <Service key={index} value1={value1} value2={value2} value3={icon} />
+        ))}
+      </div>
       <h2 className='font-bold text-3xl mt-10 text-center'>Trending Products</h2>
-    <div className='h-auto w-auto flex flex-row max-sm:flex-col justify-evenly max-sm:justify-center max-sm:flex-wrap items-center max-sm:m-10 py-8'>
-    <TrendProduct category={category[0]}  productName={productName[0]}    image={img[0]} productPrice = {productPrice['Assorted Coffee']} />
-    <TrendProduct category={category[1]}  productName={productName[1]}   image={img[1]}  productPrice = {productPrice['Hand Sanitizer']} />
-    <TrendProduct category={category[0]}  productName={productName[2]}   image={img[2]}  productPrice = {productPrice['Handpicked redchillies']} />
-    <TrendProduct category={category[0]}  productName={productName[3]}   image={img[3]}  productPrice = {productPrice['Natural Extracted Edible Oil']} />
-    </div>
-    <div
-    className=' flex justify-around w-full py-6 max-sm:flex-wrap h-auto bg-gray-200'>
-    <FeatureSection products= {products[0]} productImg={productImg[0]}/>
-    <FeatureSection products= {products[1]} productImg={productImg[1]}/>
-    <FeatureSection products= {products[2]} productImg={productImg[2]}/>
-    </div>
-    <OfferSection/>
+      <div className='h-auto w-auto flex flex-row max-sm:flex-col justify-evenly max-sm:justify-center max-sm:flex-wrap items-center max-sm:m-10 py-8'>
+        {Object.entries(PRODUCT_DATA).map(([productName, { category, img, price }]) => (
+          <TrendProduct
+            key={productName}
+            category={category}
+            productName={productName}
+            image={img}
+            productPrice={price}
+          />
+        ))}
+      </div>
+      <div className='flex justify-around w-full py-6 max-sm:flex-wrap h-auto bg-gray-200'>
+        {FEATURE_DATA.map(({ product, img }, index) => (
+          <FeatureSection key={index} products={product} productImg={img} />
+        ))}
+      </div>
+      <OfferSection />
       <h2 className='mt-10 font-serif text-3xl text-center font-medium'>Customers Reviews</h2>
-    <div className='flex justify-around max-sm:flex-wrap py-5  h-auto bg-white'>
-    <CustomerReview custName={custName[0]} clients={clients[1]}/>
-    <CustomerReview custName={custName[1]} clients={clients[0]} />
-    <CustomerReview custName={custName[1]} clients={clients[0]}/>
-    </div>
-    <LogoContainer/>
-    <Footer/>
+      <div className='flex justify-around max-sm:flex-wrap py-5  h-auto bg-white'>
+        {CUSTOMER_REVIEW_DATA.map(({ custName, clients }, index) => (
+          <CustomerReview key={index} custName={custName} clients={clients} />
+        ))}
+      </div>
+      <LogoContainer />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
