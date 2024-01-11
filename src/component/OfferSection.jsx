4be35b6c1417a617a544758d9ab49be6cf6dc2fb
Umar farooq <ufservices465@gmail.com>
2024-01-11 19:29:31 +0530

    btn fixed

--- src/component/OfferSection.jsx
+++ src/component/OfferSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import Btn from './btn'
+import Btn from './Btn'
 
 function OfferSection() {
   const [discount,setDiscount] = useState(25)
