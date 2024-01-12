41739ede92fe89ed34b123a6d6bae5e103cf46b8
Umar farooq <ufservices465@gmail.com>
2024-01-12 23:52:25 +0530

    changes to ui

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,7 @@ function App() {
         ))}
       </div>
       <h2 className='font-bold text-3xl max-sm:text-xl py-10 text-center font-serif dark:text-white dark:bg-black'>Trending Products</h2>
-      <div className='h-auto w-full flex flex-row max-sm:flex-col justify-evenly max-sm:justify-center max-sm:flex-wrap items-center max-sm:mx-10 py-8 dark:bg-black'>
+      <div className='h-auto w-auto flex flex-row max-sm:flex-col justify-evenly max-sm:justify-center max-sm:flex-wrap items-center max-sm:px-10 py-8 dark:bg-black'>
         {Object.entries(PRODUCT_DATA).map(([productName, { category, img, price }]) => (
           <TrendProduct
             key={productName}
--- src/component/TrendProduct.jsx
+++ src/component/TrendProduct.jsx
@@ -6,9 +6,9 @@ function TrendProduct({ category, productName, image, productPrice }) {
 
   return (
     <>
-      <div className='dark:dark:bg-[#1F1E1E] dark:text-white mt-5 h-auto mx-4 max-sm:mx-0 bg-white hover:bg-slate-50 hover:cursor-pointer flex flex-col justify-center items-center rounded-2xl shadow-2xl max-sm:w-full  border-black '>
+      <div className='dark:dark:bg-[#1F1E1E] dark:text-white mt-5 h-auto mx-4 max-sm:mx-0 bg-white hover:bg-slate-50 hover:cursor-pointer flex flex-col justify-center items-center rounded-2xl shadow-2xl max-sm:w-auto  border-black '>
         <img
-          className='h-auto max w-full rounded-t-2xl'
+          className=' h-auto max w-full rounded-t-2xl'
           src={`https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/${image}-300x300.jpg`}
           alt={`Product: ${productName}`}
         />
