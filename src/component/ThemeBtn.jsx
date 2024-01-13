97811190353eb5f96d88cf68fcd910a4edf0ccd9
Umar farooq <ufservices465@gmail.com>
2024-01-13 23:23:07 +0530

    footer updated

--- src/component/ThemeBtn.jsx
+++ src/component/ThemeBtn.jsx
@@ -23,7 +23,7 @@ export default function ThemeBtn() {
                 onChange={onChangeBtn}
                 checked={themeMode=== "dark"}
             />
-            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#8bc34a]"></div>
             
         </label>
 
