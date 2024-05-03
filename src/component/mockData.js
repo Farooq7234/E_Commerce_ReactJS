a7fbb0016a48682b50ec986e736eb260bde10109
Umar Farooq <ufservices465@gmail.com>
2024-05-03 22:21:27 +0530

    groceries update
    created product card for groceries page

---
+++ src/component/mockData.js
@@ -0,0 +1,55 @@
+
+
+const products =[
+    {   
+        productImage:'https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/coffee-asorted-300x300.jpg',
+        category:"Groceries",
+        productName:"Assorted Coffee",
+        price:90
+    },
+    {   
+        productImage:'https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/cashew-butter-500-300x300.jpg',
+        category:"Groceries",
+        productName:"Cashew Butter",
+        price:20
+    },
+    {   
+        productImage:'https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/diabetic-cookies-300x300.jpg',
+        category:"Groceries",
+        productName:"Diabetic Cookies",
+        price:50
+    },
+    {   
+        productImage:'https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/organic-honey-300x300.jpg',
+        category:"Groceries",
+        productName:"Organic Honey",
+        price:90
+    },
+    {   
+        productImage:'https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/edible-oil-300x300.jpg',
+        category:"Groceries",
+        productName:"Edible Oil",
+        price:80
+    },
+    {   
+        productImage:'https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/sanitizer-300x300.jpg',
+        category:"Groceries",
+        productName:"Sanitizer",
+        price:15
+    },
+    {   
+        productImage:'https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/red-chillies-300x300.jpg',
+        category:"Groceries",
+        productName:"Red Chillies",
+        price:56
+    },
+    {   
+        productImage:'https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/face-wash-300x300.jpg',
+        category:"Groceries",
+        productName:"Face Wash",
+        price:10
+    },
+]
+
+
+export {products}
